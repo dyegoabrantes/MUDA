503dc93b241078a6c01b97c3d458cfe0c3eea090
dyegoabrantes <dyegoabrantes@gmail.com>
2018-05-07 12:08:30 -0300

    finalização de desafios

--- src/pages/desafio-modal/desafio-modal.ts
+++ src/pages/desafio-modal/desafio-modal.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 import { DesafiosPage } from '../desafios/desafios';
+import { DesafioService } from './../desafios/desafios.service'
 
 @IonicPage()
 @Component({
@@ -8,22 +9,46 @@ import { DesafiosPage } from '../desafios/desafios';
   templateUrl: 'desafio-modal.html',
 })
 export class DesafioModalPage {
+
+  id: number = this.navParams.get('id');
   titulo: string = this.navParams.get('titulo');
   descricao: string = this.navParams.get('descricao');
+	categoria: String = this.navParams.get('categoria');
+	pontuacao: Number = this.navParams.get('pontuacao');
+	duracao: Number = this.navParams.get('duracao');
+	status: String = this.navParams.get('status');
+	arquivo?: Object = this.navParams.get('arquivo');
+	longitude?: String = this.navParams.get('longitude');
+  latitude?: String = this.navParams.get('latitude');
   
-  id: string = this.navParams.get('id');
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
-              public viewCtrl: ViewController) {
+              public viewCtrl: ViewController,
+              public desafioService: DesafioService) {
   }
 
-
+  cancelaDesafio(){
+    let id = this.id;
+    let obj = this.desafioService.desafios.find(function (obj) { return obj.id === id }); 
+    obj.status = 'notyet';
+    this.closeModal();
+    console.dir(obj)
+  }
   closeModal() {
     this.viewCtrl.dismiss();
   }
-
+  desfioStatus = '';
   ionViewDidLoad() {
-    console.log('ionViewDidLoad DesafioModalPage');
+    if (this.status == 'notyet'){
+      this.desfioStatus ='notyet';
+    }else{
+      if (this.status == 'pendente') {
+        this.desfioStatus ='pendente';
+      }else{
+        this.desfioStatus ='concluido';
+      }
+    }
+    console.log(this,this.desfioStatus)
   }
 
 }
