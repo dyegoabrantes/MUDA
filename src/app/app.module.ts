f31a4ebfae5141e8572fabf1f4424a593af27a70
dyegoabrantes <dyegoabrantes@gmail.com>
2018-06-11 08:15:14 -0300

    Sprint6 - Abortar Serviços de locaclização

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,6 @@ import { DesafioService } from './../pages/desafios/desafios.service';
 import { HttpModule } from '@angular/http';
 import { SocialSharing } from '@ionic-native/social-sharing';
 import { Geolocation } from '@ionic-native/geolocation';
-import { MundoPage } from './../pages/mundo/mundo';
 import { MundoPageModule } from './../pages/mundo/mundo.module';
 
 
