6a33fc647a0fda4ba6ec51a649437271c2ba9b25
dyegoabrantes <dyegoabrantes@gmail.com>
2018-06-11 08:44:31 -0300

    tratativa de erro

--- servidor/config/express.js
+++ servidor/config/express.js
@@ -3,7 +3,8 @@ let bodyParser = require('body-parser');
 let path = require('path');
 
 let usersRouter = require('../app/routes/users.routes');
-let postsRouter = require('../app/routes/posts.routes');
+let authRouter = require('../app/routes/auth.routes');
+
 
 module.exports = function () {
     let app = express();
@@ -12,7 +13,7 @@ module.exports = function () {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}))
     usersRouter(app);
-    postsRouter(app);
+    authRouter(app);
 
     return app;
 }
